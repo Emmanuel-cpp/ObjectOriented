b31addbd41bb666d213fd8289534775702ec3d49
Emmanuel Siamoonga <emmanuelsiamoonga@gmail.com>
2025-01-25 11:03:25 +0200

    Information Communication

--- Student.java
+++ Student.java
@@ -1,9 +1,15 @@
 public class Student {
     public static void main(String[] args){
         System.out.println("TODAY IS A WONDERFUL DAY THE LORD HAS GIVEN US");
+
+        //MainStudent object1MainStudent = new MainStudent(22107671, "Emmanuel Siamoonga", "COMPUTER SCIENCE");
+        //object1MainStudent.offered();
+        //object1MainStudent.registered();
+        UnderGraduate undergrad = new UnderGraduate("FULLTIME",3, 7, 22107671, "JOEL CHABOTA", "AVIATION");
+        undergrad.offered();
+        System.out.println(String.format("DEAR %s YOU ARE UNDERGRADE ID IS %d, YEAR %d NO COURSES %d ",undergrad.getName(), undergrad.getStudentId(), undergrad.getYear(), undergrad.getCourse()));
     }
 }
-
 //THIS IS THE PARENT CLASS
 class MainStudent{
     //ATTRIBUTES
@@ -19,14 +25,13 @@ class MainStudent{
     }
     //THESE ARE SOME METHODS
     public void offered(){
-        System.out.println("CONGRATULATIONS "+this.name+" YOU HAVE BEEN OFFERED AN INTERNSHIP AT THE BANK OF ZAMBIA AS A SOFTWARE ENGINEER");
+        System.out.println("CONGRATULATIONS "+this.name.toUpperCase()+" YOU HAVE BEEN OFFERED AN INTERNSHIP AT THE BANK OF ZAMBIA AS A SOFTWARE ENGINEER");
     }
     public void registered(){
-        System.out.println("NAME: "+ name);
+        System.out.println("NAME: "+ name.toUpperCase());
         System.out.println("STUDENT ID: "+ studentID);
-        System.out.println("PROGRAM: "+this.program);
+        System.out.println("PROGRAM: "+this.program.toUpperCase());
     }
-
     //SINCE OUR ATTRIBUTES ARE PRIVATE THEY CANNOT BE ACCESSED OUTSIDE THE CLASS
     //BUT WE CAN CREATE GETTERS AND SETTERS TO ENABLE OUSIDE OF CLASS ACCESS
 
@@ -37,7 +42,6 @@ class MainStudent{
     public void setStudentId(int studID){
         studentID=studID;
     }
-
     //GETTER AND SETTER FOR NAME ATTRIBUTE
     public void setName(String name){
         this.name = name;
@@ -45,7 +49,6 @@ class MainStudent{
     public String getName(){
         return name;
     }
-
     //GETTER AND SETTER FOR PROGRAM ATTRIBUTE
     public void setProgram(String myprogram){
         program=myprogram;
@@ -54,21 +57,34 @@ class MainStudent{
         return program;
     }
 }
-
 //CLASS FOR UNDERGRADUATE STUDENTS
-class UnderGraduate{
+class UnderGraduate extends MainStudent{
     //ATTRIBUTES 
     private String modeOfStudy;
     private int yearOfStudy;
     private int courses;
     //CONSTRUCTOR
-    public UnderGraduate(String modeOfStudy, int yearOfStudy, int courses){
+    public UnderGraduate(String modeOfStudy, int yearOfStudy, int courses, int studentID, String name, String program){
+        super(studentID, name, program);
         this.modeOfStudy = modeOfStudy;
         this.yearOfStudy = yearOfStudy;
         this.courses = courses;
     }
-}
+    public void setCourse(int mycourse){
+        courses = mycourse;
+    }
+    public int getCourse(){
+        return courses;
+    }
 
+    public void setYear(int year){
+        this.yearOfStudy = year;
+    }
+    public int getYear(){
+        return yearOfStudy;
+    }
+
+}
 class PostGraduate{
     private String category;
     private String project;
